8e0e3f9f8076504f0e8222bb95dc768fefd81bbd
Yuk1n0 <39364985+Yuk1n0@users.noreply.github.com>
2019-11-19 19:32:13 +0800

    Minor improvement

--- Cloud_Computing/main.c
+++ Cloud_Computing/main.c
@@ -21,47 +21,70 @@ int main(void)
     clock_t starttime, endtime;
     double cost;
     starttime = clock();
+
     FILE *fp = NULL;
     char filename[] = "./data.dat";
     char str[50] = {0};
     Word *head = NULL;
     head = (Word *)malloc(sizeof(Word));
+
     if (head == NULL)
     {
-        printf("Can't initialize node !");
+        printf("Can't initialize node !\n");
         exit(0);
     }
     memset(head, 0, sizeof(Word));
+
     if ((fp = fopen(filename, "r")) == NULL)
     {
-        printf("Program can't open this file !");
+        printf("Program can't open this file !\n");
         exit(0);
     }
+
     while (fscanf(fp, "%s", str) != EOF)
     {
         add(head, str);
     }
-    printf("头指针地址为:%p ,文件指针为地址:%p ,数组首地址为:%p\n", (void *)head, (void *)fp, (void *)str);
     fclose(fp);
-    display(head);
+
+    /*display(head);*/
     sort(head);
     release(head);
+
     endtime = clock();
     cost = (double)(endtime - starttime) / CLOCKS_PER_SEC;
-    printf("程序运行时间为: %lf秒\n", cost);
+    printf("Runtime: %lf sec\n", cost);
     return 0;
 }
 
 void add(Word *head, char *str)
 {
     char *test = str;
-    for (test = str; (int)*test != 0; test++)
+    char *index = str + 1;
+
+    if ((int)*test >= 32 && (int)*test <= 47)
     {
-        if ((int)*test >= 32 && (int)*test <= 47)
+        *test = 0;
+        int i = 0;
+        for (i = 0; i < 49; i++)
         {
-            *test = 0;
+            *test = *index;
+            test = test + 1;
+            index = index + 1;
         }
     }
+
+    for (test = str; (int)*test != 0; test++)
+    {
+        if ((int)*(test + 1) == 0)
+        {
+            if (((int)*test >= 32 && (int)*test <= 47) || ((int)*test >= 58 && (int)*test <= 64))
+            {
+                *test = 0;
+            }
+        }
+    }
+
     Word *newnode;
     Word *p = head;
     while (true)
@@ -71,7 +94,7 @@ void add(Word *head, char *str)
             p->num++;
             return;
         }
-        else if (strcmp((p->word) + 1, str + 1) == 0)
+        else if (abs(((int)*(p->word) - (int)*str)) == 32 && *(str + 1) != 0 && strcmp((p->word) + 1, str + 1) == 0)
         {
             p->num++;
             return;
@@ -97,8 +120,9 @@ void add(Word *head, char *str)
 
 void sort(Word *head)
 {
+    int i;
     Word *traversal, *tempnode, *prenode;
-    for (int i = 0; i < 3; i++)
+    for (i = 0; i < 3; i++)
     {
         traversal = head;
         tempnode = head;
